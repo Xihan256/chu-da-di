fd7400690e356346642d50714464741c56cbea29
xihan <77563304+Xihan256@users.noreply.github.com>
2023-07-04 09:23:28 +0800

    Delete Assets directory

--- Assets/lln/ChuDaDi_MainLogic/cardLogic/Suit.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace lln.ChuDaDi_MainLogic.cardLogic
-{
-    public class Suit{
-        public static int FANG_KUAI = 1;
-        public static int MEI_HUA = 2;
-        public static int HONG_TAO = 3;
-        public static int HEI_TAO = 4;
-    }
-}
--- Assets/script/NameManager.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace script{
-    public class NameManager{
-        public static string selfName;
-    }
-}
